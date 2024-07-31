d3b631a321a7f733b7ecf895a7f3ad4a152554b3
Naz <mercy.kamau@student.moringaschool.com>
2024-07-31 18:08:22 +0300

    running tests

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,7 +1,11 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
-
+<body>
+  <div>
+    <h1>please pass this test</h1>
+  </div>
+</body>
 import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
